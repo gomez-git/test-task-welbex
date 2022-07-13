55cfeef6d95355e6c2c511dfb9bd37c3fd651172
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-13 15:52:17 +0300

    init: sequelize connection config

---
+++ users/db/config.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+export default {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'postgres',
+    password: 'root',
+    database: 'welbex_users_test',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
+};
