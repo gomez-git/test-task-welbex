72f26ad224035d874ea84ba1ab241e987a0b79fa
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-13 21:08:52 +0300

    init: users crud

---
+++ users/controllers/users.js
@@ -0,0 +1,31 @@
+import * as UsersDAO from '../dao/users.js';
+
+export const read = async (_req, res) => {
+  try {
+    const user = await UsersDAO.read(res.locals.user);
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    await UsersDAO.update(req.body, res.locals.user);
+
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const deleteUser = async (_req, res) => {
+  try {
+    await UsersDAO.deleteUser(res.locals.user);
+
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
