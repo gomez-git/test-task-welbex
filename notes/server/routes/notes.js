0f2da433b9adc3344b70e45d05a93b3393359a21
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-14 18:58:53 +0300

    init: notes router

---
+++ notes/server/routes/notes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import notesController from '../controllers/notes.js';
+
+const router = new Router();
+
+router.route('/:id(\\d+)')
+  .get(notesController.read)
+  .patch(notesController.update)
+  .delete(notesController.deleteNote);
+router.route('/')
+  .get(notesController.readAll)
+  .post(notesController.create);
+
+export default router;
