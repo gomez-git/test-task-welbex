613bddd82fae9942397e247f7c79cce563755c09
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-14 19:38:16 +0300

    init: notes dao

---
+++ notes/server/dao/notes.js
@@ -0,0 +1,17 @@
+import models from '../models/index.js';
+
+const { Note } = models;
+
+export const create = ({ title, body }, { id }) => Note.create({ user_id: id, title, body });
+
+export const readAll = ({ id }) => Note.findAll({ where: { user_id: id } });
+
+export const read = ({ id }, { id: userId }) => Note.findOne({ where: { id, user_id: userId } });
+
+export const update = ({ body: { title, body }, params: { id } }, { id: userId }) => (
+  Note.update({ title, body }, { where: { id, user_id: userId } })
+);
+
+export const deleteNote = ({ id }, { id: userId }) => (
+  Note.destroy({ where: { id, user_id: userId } })
+);
