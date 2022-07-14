b50b52fef382d66a34440b41e40c2060c5129cc2
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-14 19:11:14 +0300

    init: Note model

---
+++ notes/server/models/Note.js
@@ -0,0 +1,28 @@
+import { Model, Sequelize } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+  class Note extends Model {}
+
+  Note.init({
+    user_id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    modelName: 'Note',
+    tableName: 'notes',
+  });
+
+  return Note;
+};
