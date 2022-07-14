695496f9165ea0267bb268401f4dcce0b48f8633
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-14 15:14:49 +0300

    init: docker connect

--- users/server.js
+++ users/server.js
@@ -2,9 +2,10 @@ import app from './server/app.js';
 import sequelize from './server/db/db.js';
 
 const port = process.env.PORT ?? 4000;
+const host = process.env.HOST;
 
 app.listen(port, async () => {
-  console.log(`Server up on http://localhost:${port}`); // eslint-disable-line no-console
+  console.log(`Server up on http://${host}:${port}`); // eslint-disable-line no-console
   await sequelize.authenticate();
   console.log('Database connected!'); // eslint-disable-line no-console
 });
