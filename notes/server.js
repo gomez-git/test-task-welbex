1925a9f6822a84631768164dbae1d453b392bb96
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-15 21:20:11 +0300

    feat: connect kafka consumer

--- notes/server.js
+++ notes/server.js
@@ -1,5 +1,6 @@
 import app from './server/app.js';
 import sequelize from './server/db/db.js';
+import consumer, { deleteMessage } from './server/kafka/consumer.js';
 
 const port = process.env.PORT ?? 4000;
 const host = process.env.DB_HOST;
@@ -8,4 +9,8 @@ app.listen(port, async () => {
   console.log(`Server up on http://${host}:${port}`); // eslint-disable-line no-console
   await sequelize.authenticate();
   console.log('Database connected!'); // eslint-disable-line no-console
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'delete-user' });
+  await deleteMessage();
+  console.log('Consumer connected!'); // eslint-disable-line no-console
 });
