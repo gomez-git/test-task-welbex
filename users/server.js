793807f1a4138aee4ab3d2c873bf39ce95994e64
Konstantin Kuzin <wearebeard@gmail.com>
2022-07-15 21:18:18 +0300

    feat: connect kafka producer

--- users/server.js
+++ users/server.js
@@ -1,5 +1,6 @@
 import app from './server/app.js';
 import sequelize from './server/db/db.js';
+import producer from './server/kafka/producer.js';
 
 const port = process.env.PORT ?? 4000;
 const host = process.env.DB_HOST;
@@ -8,4 +9,6 @@ app.listen(port, async () => {
   console.log(`Server up on http://${host}:${port}`); // eslint-disable-line no-console
   await sequelize.authenticate();
   console.log('Database connected!'); // eslint-disable-line no-console
+  await producer.connect();
+  console.log('Producer connected!'); // eslint-disable-line no-console
 });
